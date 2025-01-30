2cf00c8e2f9ef0807d9aa509adfc48b0ae4eb292
Vanessa Tanaka <vanessatanaka@live.com>
2025-01-29 21:50:14 -0300

    projeto inicial

--- app.js
+++ app.js
@@ -27,9 +27,9 @@ function verificarChute() {
         document.getElementById('reiniciar').removeAttribute('disabled');
     } else {
         if (chute > numeroSecreto) {
-            exibirTextoNaTela('p', 'O número secreto é menor');
+            exibirTextoNaTela('p', `O número secreto é menor do que ${chute}`);
         } else {
-            exibirTextoNaTela('p', 'O número secreto é maior');
+            exibirTextoNaTela('p', `O número secreto é maior do que ${chute}`);
         }
         tentativas++;
         limparCampo();
